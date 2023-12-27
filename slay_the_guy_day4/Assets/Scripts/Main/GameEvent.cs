3175a8366b018dd992f3ed145f18396636f40af2
TakedaNaomichi <takeda.naomichi@donuts.ne.jp>
2023-12-27 10:12:56 +0900

    基盤

---
+++ slay_the_guy_day4/Assets/Scripts/Main/GameEvent.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvent
+{
+    Action<GameObject> AddComponentList;
+    Action<GameObject> RemoveComponentList;
+}
---
+++ slay_the_guy_day4/Assets/Scripts/Main/Main.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    private GameEvent gameEvent;
+
+    void Start()
+    {
+        gameEvent = new GameEvent();
+    }
+
+    void Update()
+    {
+
+    }
+}
